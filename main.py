50adc4cb5d819f43bc0d113d5cd1b7a459a482ec
Oláh György <gyuri7531246@gmail.com>
2023-12-16 23:24:00 +0100

    DayNine -twoStar

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# This is a sample Python script.
-import copy
-from copy import deepcopy
-
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 def get_next(array):
     summed_array = [array[i + 1] - array[i] for i in range(0, len(array) - 1)]
     if all(element == summed_array[0] for element in summed_array):
@@ -20,25 +6,21 @@ def get_next(array):
         return array[-1] + get_next(summed_array)
 
 
-def sum(array):
-    sum = 0
-    for i in array:
-        sum = sum + i
-    return sum
+def get_previus(array):
+    summed_array = [array[i + 1] - array[i] for i in range(0, len(array) - 1)]
+    if all(element == summed_array[0] for element in summed_array):
+        return array[0] - summed_array[0]
+    else:
+        return array[0] - get_previus(summed_array)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     with open("input/input.txt", "r") as f:
         lines = f.read().split("\n")
     numberoflines = [[int(num) for num in line.split(" ")] for line in lines]
-    print(numberoflines)
-    sumedArray = [get_next(x) for x in numberoflines]
 
+    sumedArray = [get_next(x) for x in numberoflines]
     print(sum(sumedArray))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-# class read_stuff():
-#     def __init__(self):
+    sumedArrayPt2 = [get_previus(x) for x in numberoflines]
+    print(sum(sumedArrayPt2))
